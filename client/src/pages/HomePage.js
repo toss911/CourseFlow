bfcb8043c8f9b3b7684597bce4047486673f98ef
Bank <bankthitivit.pst@gmail.com>
2022-11-02 00:11:44 +0700

    feat: add tag at instructor section

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,25 +1,30 @@
 function HomePage() {
-  return; //Nav section
-  //Start Codind here [Sun]
-  //-----------------------End Nav section-------------------------//
-  //Hero section
-  //Start Codind here [Sun]
-  //-----------------------End Hero section-------------------------//
-  //Features section
-  //Start Codind here [Eye]
-  //-----------------------End Features section-------------------------//
-  //Instructor section
-  //Start Codind here [Bank]
-  //-----------------------End Instructor section-------------------------//
-  //Review section
-  //Start Codind here [Bank]
-  //-----------------------End Review section-------------------------//
-  //Pre-Footer section
-  //Start Codind here [Chain]
-  //-----------------------End Pre-Footer section-------------------------//
-  //Footer section
-  //Start Codind here [Chain]
-  //-----------------------End Footer section-------------------------//
+  return (
+    //Nav section
+    //Start Codind here [Sun]
+    //-----------------------End Nav section-------------------------//
+    //Hero section
+    //Start Codind here [Sun]
+    //-----------------------End Hero section-------------------------//
+    //Features section
+    //Start Codind here [Eye]
+    //-----------------------End Features section-------------------------//
+    //Instructor section
+    <div>
+      <h1>Our Professional Intructor</h1>
+      <div>picture1</div>
+    </div>
+    //-----------------------End Instructor section-------------------------//
+    //Review section
+    //Start Codind here [Bank]
+    //-----------------------End Review section-------------------------//
+    //Pre-Footer section
+    //Start Codind here [Chain]
+    //-----------------------End Pre-Footer section-------------------------//
+    //Footer section
+    //Start Codind here [Chain]
+    //-----------------------End Footer section-------------------------//
+  );
 }
 
 export default HomePage;
