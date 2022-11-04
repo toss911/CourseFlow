85e59f70143e3a1a5a8d31392cd3dd0449c3aa04
Bank <bankthitivit.pst@gmail.com>
2022-11-04 10:40:23 +0700

    feat: add auth font-end

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -20,7 +20,17 @@ import { useNavigate } from "react-router-dom";
 function RegisterPage() {
   const navigate = useNavigate();
 
-  const [value, onChange] = useState(new Date());
+  const { register } = useAuth();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      username,
+      password,
+      firstName,
+      lastName,
+    };
+    register(data);
+  };
   const [inputName, setInputName] = useState("");
   const [inputDate, setInputDate] = useState("");
   const [inputEducate, setInputEducate] = useState("");
@@ -66,6 +76,7 @@ function RegisterPage() {
       setIsErrorEmail(true);
     }
   };
+
   const handlePasswordChange = (e) => {
     setInputPassword(e.target.value);
     if (/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(e.target.value)) {
@@ -104,161 +115,163 @@ function RegisterPage() {
             Register to start learning!
           </Heading>
           {/* //------------------------- Input Name --------------------// */}
-          <FormControl isInvalid={isErrorName} isRequired>
-            <Flex flexDirection="column" justifyContent="flex-start">
-              <FormLabel variant="body2" color="black" pt="37px">
-                Name
-              </FormLabel>
-              <Input
-                type="text"
-                w="453px"
-                h="48px"
-                placeholder="Enter First Name and Last Name"
-                value={inputName}
-                onChange={handleNameChange}
-              />
-              {!isErrorName ? (
-                <FormHelperText>&nbsp;</FormHelperText>
-              ) : (
-                <FormErrorMessage>
-                  Name must only contain alphabets and some special characters
-                  <br />
-                  (e.g., comma, dot, apostrophe, and hyphen)
-                </FormErrorMessage>
-              )}
-              {/* //-------------------------- Input Date --------------------// */}
-              <FormControl isInvalid={isErrorDate} isRequired>
-                <FormLabel variant="body2" color="black" pt="20px">
-                  Date of Birth
-                </FormLabel>
-                <div
-                  style={{
-                    position: "relative",
-                    float: "left",
-                  }}
-                >
-                  <Input
-                    color="#9AA1B9"
-                    type="date"
-                    w="453px"
-                    h="48px"
-                    placeholder="MM/DD/YYYY"
-                    value={inputDate}
-                    onChange={handleDateChange}
-                    sx={{
-                      "::-webkit-datetime-edit-year-field": {
-                        color: "",
-                      },
-                    }}
-                  />
-                  <span
-                    style={{
-                      position: "absolute",
-                      top: "15px",
-                      right: "20px",
-                      width: "21px",
-                      height: "21px",
-                      background: "#fff",
-                      pointerEvents: "none",
-                    }}
-                  >
-                    <button
-                      style={{
-                        border: "none",
-                        background: "transparen",
-                      }}
-                    >
-                      <Image
-                        src="/assets/register-page/icons-calendar.svg"
-                        alt="background-image"
-                      />
-                    </button>
-                  </span>
-                  {!isErrorDate ? (
-                    <FormHelperText>&nbsp;</FormHelperText>
-                  ) : (
-                    <FormErrorMessage>
-                      Date of birth is required.
-                    </FormErrorMessage>
-                  )}
-                </div>
-              </FormControl>
-              {/* //---------------------- Input Educational --------------------// */}
-              <FormControl isInvalid={isErrorEducate} isRequired>
-                <FormLabel variant="body2" color="black" pt="20px">
-                  Educational Background
+          <form onSubmit={handleSubmit}>
+            <FormControl isInvalid={isErrorName} isRequired>
+              <Flex flexDirection="column" justifyContent="flex-start">
+                <FormLabel variant="body2" color="black" pt="37px">
+                  Name
                 </FormLabel>
                 <Input
                   type="text"
                   w="453px"
                   h="48px"
-                  placeholder="Enter Educational Background"
-                  value={inputEducate}
-                  onChange={handleEducateChange}
+                  placeholder="Enter First Name and Last Name"
+                  value={inputName}
+                  onChange={handleNameChange}
                 />
-                {!isErrorEducate ? (
+                {!isErrorName ? (
                   <FormHelperText>&nbsp;</FormHelperText>
                 ) : (
                   <FormErrorMessage>
-                    Educational background is required.
+                    Name must only contain alphabets and some special characters
+                    <br />
+                    (e.g., comma, dot, apostrophe, and hyphen)
                   </FormErrorMessage>
                 )}
-              </FormControl>
-              {/* //------------------------- Input Email --------------------// */}
-              <FormControl isInvalid={isErrorEmail} isRequired>
-                <FormLabel variant="body2" color="black" pt="20px">
-                  Email
-                </FormLabel>
-                <Input
-                  type="email"
-                  w="453px"
-                  h="48px"
-                  placeholder="Enter Email"
-                  value={inputEmail}
-                  onChange={handleEmailChange}
-                />
-                {!isErrorEmail ? (
-                  <FormHelperText>&nbsp;</FormHelperText>
-                ) : (
-                  <FormErrorMessage>
-                    Email should be in this form: "john@mail.com".
-                  </FormErrorMessage>
-                )}
-              </FormControl>
-              {/* //------------------------- Input Password --------------------// */}
-              <FormControl isInvalid={isErrorPassword} isRequired>
-                <FormLabel variant="body2" color="black" pt="20px">
-                  Password
-                </FormLabel>
-                <Input
-                  type="password"
-                  w="453px"
-                  h="48px"
-                  placeholder="Enter Password"
-                  value={inputPassword}
-                  onChange={handlePasswordChange}
-                />
-                {!isErrorPassword ? (
-                  <FormHelperText>&nbsp;</FormHelperText>
-                ) : (
-                  <FormErrorMessage>
-                    Password must have minimum eight characters, at least one
-                    letter <br /> and one number
-                  </FormErrorMessage>
-                )}
-              </FormControl>
-              {/* //------------------------- Register Button --------------------// */}
-              <Button variant="primary" mt="40px" w="453px" h="60px">
-                Register
-              </Button>
-              <Text as="b" mt="44px">
-                Already have an account?
-                <Link ml="12px" onClick={() => navigate("/login")}>
-                  Log in
-                </Link>
-              </Text>
-            </Flex>
-          </FormControl>
+                {/* //-------------------------- Input Date --------------------// */}
+                <FormControl isInvalid={isErrorDate} isRequired>
+                  <FormLabel variant="body2" color="black" pt="20px">
+                    Date of Birth
+                  </FormLabel>
+                  <div
+                    style={{
+                      position: "relative",
+                      float: "left",
+                    }}
+                  >
+                    <Input
+                      color="#9AA1B9"
+                      type="date"
+                      w="453px"
+                      h="48px"
+                      placeholder="MM/DD/YYYY"
+                      value={inputDate}
+                      onChange={handleDateChange}
+                      sx={{
+                        "::-webkit-datetime-edit-year-field": {
+                          color: "",
+                        },
+                      }}
+                    />
+                    <span
+                      style={{
+                        position: "absolute",
+                        top: "15px",
+                        right: "20px",
+                        width: "21px",
+                        height: "21px",
+                        background: "#fff",
+                        pointerEvents: "none",
+                      }}
+                    >
+                      <button
+                        style={{
+                          border: "none",
+                          background: "transparen",
+                        }}
+                      >
+                        <Image
+                          src="/assets/register-page/icons-calendar.svg"
+                          alt="background-image"
+                        />
+                      </button>
+                    </span>
+                    {!isErrorDate ? (
+                      <FormHelperText>&nbsp;</FormHelperText>
+                    ) : (
+                      <FormErrorMessage>
+                        Date of birth is required.
+                      </FormErrorMessage>
+                    )}
+                  </div>
+                </FormControl>
+                {/* //---------------------- Input Educational --------------------// */}
+                <FormControl isInvalid={isErrorEducate} isRequired>
+                  <FormLabel variant="body2" color="black" pt="20px">
+                    Educational Background
+                  </FormLabel>
+                  <Input
+                    type="text"
+                    w="453px"
+                    h="48px"
+                    placeholder="Enter Educational Background"
+                    value={inputEducate}
+                    onChange={handleEducateChange}
+                  />
+                  {!isErrorEducate ? (
+                    <FormHelperText>&nbsp;</FormHelperText>
+                  ) : (
+                    <FormErrorMessage>
+                      Educational background is required.
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                {/* //------------------------- Input Email --------------------// */}
+                <FormControl isInvalid={isErrorEmail} isRequired>
+                  <FormLabel variant="body2" color="black" pt="20px">
+                    Email
+                  </FormLabel>
+                  <Input
+                    type="email"
+                    w="453px"
+                    h="48px"
+                    placeholder="Enter Email"
+                    value={inputEmail}
+                    onChange={handleEmailChange}
+                  />
+                  {!isErrorEmail ? (
+                    <FormHelperText>&nbsp;</FormHelperText>
+                  ) : (
+                    <FormErrorMessage>
+                      Email should be in this form: "john@mail.com".
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                {/* //------------------------- Input Password --------------------// */}
+                <FormControl isInvalid={isErrorPassword} isRequired>
+                  <FormLabel variant="body2" color="black" pt="20px">
+                    Password
+                  </FormLabel>
+                  <Input
+                    type="password"
+                    w="453px"
+                    h="48px"
+                    placeholder="Enter Password"
+                    value={inputPassword}
+                    onChange={handlePasswordChange}
+                  />
+                  {!isErrorPassword ? (
+                    <FormHelperText>&nbsp;</FormHelperText>
+                  ) : (
+                    <FormErrorMessage>
+                      Password must have minimum eight characters, at least one
+                      letter <br /> and one number
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                {/* //------------------------- Register Button --------------------// */}
+                <Button variant="primary" mt="40px" w="453px" h="60px">
+                  Register
+                </Button>
+                <Text as="b" mt="44px">
+                  Already have an account?
+                  <Link ml="12px" onClick={() => navigate("/login")}>
+                    Log in
+                  </Link>
+                </Text>
+              </Flex>
+            </FormControl>
+          </form>
         </Flex>
       </Flex>
     </Box>
