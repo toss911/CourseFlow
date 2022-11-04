bd56f51796c398aa50070b285c5537f85d221648
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-11-04 10:05:07 +0700

    initialized feat view courses

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,7 +4,9 @@ import { pool } from "../utils/db.js";
 const coursesRouter = Router();
 
 coursesRouter.get("", async (req, res) => {
-    `select courses.name, courses.summary, courses.cover_image_directory, courses.learning_time, lessons.name, count(lessons.lesson_id), lessons.course_id from courses 
+    `select courses.name, courses.summary, courses.cover_image_directory, courses.learning_time, count(lessons.lesson_id), lessons.course_id from courses 
     inner join lessons on courses.course_id = lessons.course_id
-    where courses.course_id = lessons.course_id`
+    group by lessons.course_id`
+
+    // this is just a sample code
 })
