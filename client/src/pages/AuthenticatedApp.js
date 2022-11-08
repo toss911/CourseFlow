ad1a770b07d5fdf3afc0a7942e4cb4e98239b5a0
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-11-08 12:30:44 +0700

    add a path to user profile page

--- client/src/pages/AuthenticatedApp.js
+++ client/src/pages/AuthenticatedApp.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage.js";
 import OurCourses from "./OurCoursesPage.js";
 import CourseDetail from "./Coursedetail.js";
+import UserProfile from "./UserProfile.js";
 
 function AuthenticatedApp() {
   return (
@@ -9,6 +10,7 @@ function AuthenticatedApp() {
       <Route path="/" element={<HomePage />} />
       <Route path="/courses" element={<OurCourses />} />
       <Route path="/courses/:id" element={<CourseDetail />} />
+      <Route path="profile" element={<UserProfile />} />
     </Routes>
   );
 }
