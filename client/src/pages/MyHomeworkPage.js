f95163ecaffcab29acf9074183ee18ee1660aeb2
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-11-14 21:23:17 +0700

    feat: add homework route [BE]

--- client/src/pages/MyHomeworkPage.js
+++ client/src/pages/MyHomeworkPage.js
@@ -28,20 +28,10 @@ function MyHomework() {
           justifyContent="center"
           backgroundImage="url('/assets/myhomework-page/background.png')"
         >
-          <Flex
-            flexDirection="column"
-            alignItems="center"
-            h="145px"
-          >
+          <Flex flexDirection="column" alignItems="center" h="145px">
             <Heading variant="headline2">My Homework</Heading>
             {/* Don't forget to change the color of active tabs to black */}
-            <Tabs
-              w="470px"
-              h="40px"
-              mt="60px"
-              gap="16px"
-              textColor="gray.600"
-            >
+            <Tabs w="470px" h="40px" mt="60px" gap="16px" textColor="gray.600">
               <TabList>
                 <Tab>
                   <Text variant="body2">All</Text>
@@ -59,7 +49,6 @@ function MyHomework() {
                   <Text variant="body2">Overdue</Text>
                 </Tab>
               </TabList>
-
               <TabPanels mt="40px">
                 <TabPanel>
                   <Center>
