95a0d3adc426f8647047b4d8b9987ada10c5dc03
Tossaporn Udomsap <tossaporn.udom@gmail.com>
2022-11-17 16:34:31 +0700

    fix(desire-course): eliminate some minor bugs

--- server/controllers/coursesController.js
+++ server/controllers/coursesController.js
@@ -171,3 +171,27 @@ export const postSubscribeOrAddCourse = async (req, res) => {
     return res.sendStatus(500);
   }
 };
+
+export const getDesire = async (req, res) => {
+  try {
+    const userId = req.query.byUser;
+    let courseDesire = await pool.query(
+      `select desired_courses.course_id, courses.course_name, courses.summary, courses.cover_image_directory, courses.learning_time, COUNT(lessons.lesson_id)
+      from desired_courses
+      inner join courses
+      on courses.course_id = desired_courses.course_id
+      inner join lessons
+      on courses.course_id = lessons.course_id
+      where desired_courses.user_id = $1
+      group by desired_courses.course_id, courses.course_id`,
+      [userId]
+    );
+    let course = courseDesire.rows;
+
+    return res.json({
+      data: course,
+    });
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+};
