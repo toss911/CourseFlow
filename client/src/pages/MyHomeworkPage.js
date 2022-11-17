98e546f56268a7baca4d76d2d0e5dc1e5c3367ec
Sundaeth <potsaweeth@gmail.com>
2022-11-17 15:30:48 +0700

    resolve rebase

--- client/src/pages/MyHomeworkPage.js
+++ client/src/pages/MyHomeworkPage.js
@@ -12,6 +12,8 @@ import {
   TabPanel,
   Text,
   Center,
+  Container,
+  Stack,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { useAuth } from "../contexts/authentication.js";
@@ -52,102 +54,76 @@ function MyHomework() {
   }, []);
 
   return (
-    <Box>
-      <Navbar />
+    <Stack direction={"column"}>
       <Box>
-        <Flex flexDirection="column" h="1560px">
+        <Navbar />
+        <Box>
           <Flex
-            mt="100px"
-            w="1418px"
-            h="190px"
-            ml="43px"
-            justifyContent="center"
-            backgroundImage="url('/assets/myhomework-page/background.png')"
+            flexDirection="column"
+            // h="1560px"
           >
-            <Flex flexDirection="column" alignItems="center" h="145px">
-              <Heading variant="headline2">My Homework</Heading>
-              <Tabs
-                w="470px"
-                h="40px"
-                mt="60px"
-                gap="16px"
-                textColor="gray.600"
-                sx={{
-                  ".css-1oezttv": {
-                    borderColor: "white",
-                    color: "#9AA1B9",
-                  },
-                  ".css-1oezttv[aria-selected=true]": {
-                    borderColor: "black",
-                    color: "black",
-                  },
-                }}
+            <Flex
+              mt="100px"
+              w="1418px"
+              // h="190px"
+              backgroundSize="100%"
+              backgroundRepeat="no-repeat"
+              ml="43px"
+              justifyContent="center"
+              backgroundImage="url('/assets/myhomework-page/background.png')"
+            >
+              <Flex
+                flexDirection="column"
+                alignItems="center"
+                // h="145px"
               >
-                <TabList>
-                  <Tab>
-                    <Text variant="body2">All</Text>
-                  </Tab>
-                  <Tab>
-                    <Text variant="body2">Pending</Text>
-                  </Tab>
-                  <Tab>
-                    <Text variant="body2">In progress</Text>
-                  </Tab>
-                  <Tab>
-                    <Text variant="body2">Submitted</Text>
-                  </Tab>
-                  <Tab>
-                    <Text variant="body2">Overdue</Text>
-                  </Tab>
-                </TabList>
-                <TabPanels mt="40px">
-                  <TabPanel>
-                    <Center>
-                      <Flex
-                        flexDirection="column"
-                        alignItems="center"
-                        w="1120px"
-                        h="1560px"
-                        mb="145px"
-                      >
-                        {homework.map((hw, key) => {
-                          return (
-                            <HomeworkBox
-                              key={key}
-                              courseName={hw.course_name}
-                              lessonName={hw.lesson_name}
-                              subLessonName={hw.sub_lesson_name}
-                              status={hw.status}
-                              hwDetail={hw.detail}
-                              daysUntilDeadline={hw.days_until_deadline}
-                              answer={hw.answer}
-                              dayOrDays={
-                                hw.days_until_deadline <= 1 ? "day" : "days"
-                              }
-                              courseId={hw.course_id}
-                              submitHomework={submitHomework}
-                              saveAnswerDraft={saveAnswerDraft}
-                              assignmentId={hw.assignment_id}
-                            />
-                          );
-                        })}
-                      </Flex>
-                    </Center>
-                  </TabPanel>
-                  <TabPanel>
-                    <Center>
-                      <Flex
-                        flexDirection="column"
-                        alignItems="center"
-                        w="1120px"
-                        h="1560px"
-                        mb="145px"
-                      >
-                        {homework
-                          .filter((hw) => {
-                            return hw.status == "pending";
-                          })
-                          .map((hw, key) => {
+                <Heading variant="headline2">My Homework</Heading>
+                {/* Don't forget to change the color of active tabs to black */}
+                <Tabs
+                  w="470px"
+                  // h="40px"
+                  mt="60px"
+                  gap="16px"
+                  textColor="gray.600"
+                  sx={{
+                    ".css-1oezttv": {
+                      borderColor: "white",
+                      color: "#9AA1B9",
+                    },
+                    ".css-1oezttv[aria-selected=true]": {
+                      borderColor: "black",
+                      color: "black",
+                    },
+                  }}
+                >
+                  <TabList>
+                    <Tab>
+                      <Text variant="body2">All</Text>
+                    </Tab>
+                    <Tab>
+                      <Text variant="body2">Pending</Text>
+                    </Tab>
+                    <Tab>
+                      <Text variant="body2">In progress</Text>
+                    </Tab>
+                    <Tab>
+                      <Text variant="body2">Submitted</Text>
+                    </Tab>
+                    <Tab>
+                      <Text variant="body2">Overdue</Text>
+                    </Tab>
+                  </TabList>
+                  <TabPanels mt="40px">
+                    <TabPanel>
+                      <Center>
+                        <Flex
+                          flexDirection="column"
+                          alignItems="center"
+                          w="1120px"
+                          // h="1560px"
+                          mb="145px"
+                        >
+                          {homework.map((hw, key) => {
                             return (
                               <HomeworkBox
                                 key={key}
@@ -161,101 +137,25 @@ function MyHomework() {
                                 dayOrDays={
                                   hw.days_until_deadline <= 1 ? "day" : "days"
                                 }
+                                courseId={hw.course_id}
                                 submitHomework={submitHomework}
-                                saveAnswerDraft={saveAnswerDraft}
                                 assignmentId={hw.assignment_id}
                               />
                             );
                           })}
-                      </Flex>
-                    </Center>
-                  </TabPanel>
-                  <TabPanel>
-                    <Center>
-                      <Flex
-                        flexDirection="column"
-                        alignItems="center"
-                        w="1120px"
-                        h="1560px"
-                        mb="145px"
-                      >
-                        {homework
-                          .filter((hw) => {
-                            return hw.status == "in progress";
-                          })
-                          .map((hw, key) => {
-                            return (
-                              <HomeworkBox
-                                key={key}
-                                courseName={hw.course_name}
-                                lessonName={hw.lesson_name}
-                                subLessonName={hw.sub_lesson_name}
-                                status={hw.status}
-                                hwDetail={hw.detail}
-                                daysUntilDeadline={hw.days_until_deadline}
-                                answer={hw.answer}
-                                dayOrDays={
-                                  hw.days_until_deadline <= 1 ? "day" : "days"
-                                }
-                                submitHomework={submitHomework}
-                                saveAnswerDraft={saveAnswerDraft}
-                                assignmentId={hw.assignment_id}
-                              />
-                            );
-                          })}
-                      </Flex>
-                    </Center>
-                  </TabPanel>
-                  <TabPanel>
-                    <Center>
-                      <Flex
-                        flexDirection="column"
-                        alignItems="center"
-                        w="1120px"
-                        h="1560px"
-                        mb="145px"
-                        pl="24px"
-                      >
-                        {homework
-                          .filter((hw) => {
-                            return hw.status == "submitted";
-                          })
-                          .map((hw, key) => {
-                            return (
-                              <HomeworkBox
-                                key={key}
-                                courseName={hw.course_name}
-                                lessonName={hw.lesson_name}
-                                subLessonName={hw.sub_lesson_name}
-                                status={hw.status}
-                                hwDetail={hw.detail}
-                                daysUntilDeadline={hw.days_until_deadline}
-                                answer={hw.answer}
-                                dayOrDays={
-                                  hw.days_until_deadline <= 1 ? "day" : "days"
-                                }
-                                saveAnswerDraft={saveAnswerDraft}
-                                submitHomework={submitHomework}
-                              />
-                            );
-                          })}
-                      </Flex>
-                    </Center>
-                  </TabPanel>
-                  <TabPanel>
-                    <Center>
-                      <Flex
-                        flexDirection="column"
-                        alignItems="center"
-                        w="1120px"
-                        h="1560px"
-                        mb="145px"
-                      >
-                        {homework
-                          .filter((hw) => {
-                            return hw.status == "overdue";
-                          })
-                          .map((hw, key) => {
+                        </Flex>
+                      </Center>
+                    </TabPanel>
+                    <TabPanel>
+                      <Center>
+                        <Flex
+                          flexDirection="column"
+                          alignItems="center"
+                          w="1120px"
+                          // h="1560px"
+                          mb="145px"
+                        >
+                          {pendingAssignments.map((hw, key) => {
                             return (
                               <HomeworkBox
                                 key={key}
@@ -273,17 +173,108 @@ function MyHomework() {
                               />
                             );
                           })}
-                      </Flex>
-                    </Center>
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
+                        </Flex>
+                      </Center>
+                    </TabPanel>
+                    <TabPanel>
+                      <Center>
+                        <Flex
+                          flexDirection="column"
+                          alignItems="center"
+                          w="1120px"
+                          // h="1560px"
+                          mb="145px"
+                        >
+                          {inProgressAssignments.map((hw, key) => {
+                            return (
+                              <HomeworkBox
+                                key={key}
+                                courseName={hw.course_name}
+                                lessonName={hw.lesson_name}
+                                subLessonName={hw.sub_lesson_name}
+                                status={hw.status}
+                                hwDetail={hw.detail}
+                                daysUntilDeadline={hw.days_until_deadline}
+                                answer={hw.answer}
+                                dayOrDays={
+                                  hw.days_until_deadline <= 1 ? "day" : "days"
+                                }
+                                assignmentId={hw.assignment_id}
+                              />
+                            );
+                          })}
+                        </Flex>
+                      </Center>
+                    </TabPanel>
+                    <TabPanel>
+                      <Center>
+                        <Flex
+                          flexDirection="column"
+                          alignItems="center"
+                          w="1120px"
+                          // h="1560px"
+                          mb="145px"
+                          pl="24px"
+                        >
+                          {submittedAssignments.map((hw, key) => {
+                            return (
+                              <HomeworkBox
+                                key={key}
+                                courseName={hw.course_name}
+                                lessonName={hw.lesson_name}
+                                subLessonName={hw.sub_lesson_name}
+                                status={hw.status}
+                                hwDetail={hw.detail}
+                                daysUntilDeadline={hw.days_until_deadline}
+                                answer={hw.answer}
+                                dayOrDays={
+                                  hw.days_until_deadline <= 1 ? "day" : "days"
+                                }
+                              />
+                            );
+                          })}
+                        </Flex>
+                      </Center>
+                    </TabPanel>
+                    <TabPanel>
+                      <Center>
+                        <Flex
+                          flexDirection="column"
+                          alignItems="center"
+                          w="1120px"
+                          // h="1560px"
+                          mb="145px"
+                        >
+                          {overdueAssignments.map((hw, key) => {
+                            return (
+                              <HomeworkBox
+                                key={key}
+                                courseName={hw.course_name}
+                                lessonName={hw.lesson_name}
+                                subLessonName={hw.sub_lesson_name}
+                                status={hw.status}
+                                hwDetail={hw.detail}
+                                daysUntilDeadline={hw.days_until_deadline}
+                                answer={hw.answer}
+                                dayOrDays={
+                                  hw.days_until_deadline <= 1 ? "day" : "days"
+                                }
+                                assignmentId={hw.assignment_id}
+                              />
+                            );
+                          })}
+                        </Flex>
+                      </Center>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </Flex>
             </Flex>
           </Flex>
-        </Flex>
+        </Box>
       </Box>
       <Footer />
-    </Box>
+    </Stack>
   );
 }
 
