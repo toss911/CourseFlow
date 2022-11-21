cdc906633fbee6c855c710ab28e17f14345dc6b1
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-11-21 22:36:19 +0700

    fix: refactored code and added a search icon in the search field

--- client/src/pages/Admin/AdminViewCoursesPage.js
+++ client/src/pages/Admin/AdminViewCoursesPage.js
@@ -17,10 +17,18 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 function AdminViewCourses() {
-
   const adminId = 3; // Get the ID from the context
   const [adminCourses, setAdminCourses] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const columnNames = [
+    "Image",
+    "Course name",
+    "Lesson",
+    "Price",
+    "Created date",
+    "Updated date",
+    "Action",
+  ];
 
   const getAdminCourses = async () => {
     const query = new URLSearchParams();
@@ -44,7 +52,6 @@ function AdminViewCourses() {
     return () => clearTimeout(getCourses);
   }, [searchText]);
 
-
   return (
     <Box>
       <Flex>
@@ -61,97 +68,58 @@ function AdminViewCourses() {
                 <Thead backgroundColor="gray.300" h="41px">
                   <Tr>
                     <Th></Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Image
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Course name
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Lesson
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Price
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Created date
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Updated date
-                      </Text>
-                    </Th>
-                    <Th>
-                      <Text
-                        variant="body3"
-                        textTransform="capitalize"
-                        textColor="gray.800"
-                      >
-                        Action
-                      </Text>
-                    </Th>
+                    {columnNames.map((columnName, key) => {
+                      return (
+                        <Th key={key}>
+                          <Text
+                            variant="body3"
+                            textTransform="capitalize"
+                            textColor="gray.800"
+                          >
+                            {columnName}
+                          </Text>
+                        </Th>
+                      );
+                    })}
                   </Tr>
                 </Thead>
                 <Tbody>
                   {adminCourses.map((course, key) => {
                     return (
-
                       <Tr key={key}>
-                        <Td>{key+1}</Td>
+                        <Td>{key + 1}</Td>
                         <Td>
-                          <Image src={course.cover_image_directory}/>
+                          <Image src={course.cover_image_directory} />
                         </Td>
                         <Td>
-                          <Text variant="body2" textColor="black">{course.course_name}</Text>
+                          <Text variant="body2" textColor="black">
+                            {course.course_name}
+                          </Text>
                         </Td>
                         <Td>
-                          <Text variant="body2" textColor="black">{course.lessons_count} Lessons</Text>
+                          <Text variant="body2" textColor="black">
+                            {course.lessons_count} Lessons
+                          </Text>
                         </Td>
                         <Td>
-                          <Text variant="body2" textColor="black">{course.price.toLocaleString("en", { minimumFractionDigits: 2 })}</Text>
+                          <Text variant="body2" textColor="black">
+                            {course.price.toLocaleString("en", {
+                              minimumFractionDigits: 2,
+                            })}
+                          </Text>
                         </Td>
                         <Td>
-                          <Text variant="body2" textColor="black">{course.created_date}</Text>
+                          <Text variant="body2" textColor="black">
+                            {course.created_date}
+                          </Text>
                         </Td>
                         <Td>
-                          <Text variant="body2" textColor="black">{course.updated_date}</Text>
+                          <Text variant="body2" textColor="black">
+                            {course.updated_date}
+                          </Text>
                         </Td>
                         <Td>
-                          <Flex gap="20px">
+                          <Flex gap="20px" w="120px">
                             <Image
                               src="../../../assets/admin-page/bin.svg"
                               alt="bin"
