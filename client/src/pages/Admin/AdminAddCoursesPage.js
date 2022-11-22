1439006ea58e7354411a68f2a6c01e7695416797
Sundaeth <potsaweeth@gmail.com>
2022-11-22 15:09:05 +0700

    feat:added upload function

--- client/src/pages/Admin/AdminAddCoursesPage.js
+++ client/src/pages/Admin/AdminAddCoursesPage.js
@@ -4,18 +4,45 @@ import {
   Stack,
   Input,
   Text,
+  Container,
+  Image,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
 import { Sidebar } from "../../components/SidebarAdmin";
 import LessonTable from "../../components/LessonsTable";
 import AdminNavbarAdd from "../../components/AdminNavbarAdd";
+let action;
 
 const AdminAddCoursesPage = () => {
+  const toast = useToast();
+  const handleFileChange = (event) => {
+    const currentFile = event.target.files[0];
+    if (currentFile) {
+      if (/jpeg|png/gi.test(currentFile.type)) {
+        if (currentFile.size <= 2e6) {
+          action = "change";
+        } else {
+          return toast({
+            title: "File size must be less than 2MB!",
+            status: "error",
+            isClosable: true,
+          });
+        }
+      } else {
+        return toast({
+          title: "File type must be JPG/PNG only!",
+          status: "error",
+          isClosable: true,
+        });
+      }
+    }
+  };
   return (
     <>
       <Flex>
         <Sidebar />
-        <Flex flexDirection="column" w="vw" >
+        <Flex flexDirection="column" w="vw">
           <AdminNavbarAdd
             heading="Add Course"
             action="Cancel"
@@ -61,12 +88,65 @@ const AdminAddCoursesPage = () => {
                     <Text variant="body2">Course detail</Text>
                     <Input placeholder="" h="192px" w="920px" />
                   </Box>
+                  <Flex
+                    align="start"
+                    flexWrap="wrap"
+                    //    w="920px"
+                  >
+                    <label>
+                      <Input type="file" hidden onChange={handleFileChange} />
+                      <Flex
+                        w="240px"
+                        h="240px"
+                        direction="column"
+                        justify="center"
+                        align="center"
+                        color="blue.400"
+                        cursor="pointer"
+                        bg="gray.100"
+                      >
+                        <Text fontSize="36px">+</Text>
+                        <Text variant="body2">Upload Image</Text>
+                      </Flex>
+                    </label>
+                    <label w="250px">
+                      <Input type="file" hidden onChange={handleFileChange} />
+                      <Flex
+                        w="240px"
+                        h="240px"
+                        direction="column"
+                        justify="center"
+                        align="center"
+                        color="blue.400"
+                        cursor="pointer"
+                        bg="gray.100"
+                      >
+                        <Text fontSize="36px">+</Text>
+                        <Text variant="body2">Upload Image</Text>
+                      </Flex>
+                    </label>
+                    <label w="250px">
+                      <Input type="file" hidden onChange={handleFileChange} />
+                      <Flex
+                        w="240px"
+                        h="240px"
+                        direction="column"
+                        justify="center"
+                        align="center"
+                        color="blue.400"
+                        cursor="pointer"
+                        bg="gray.100"
+                      >
+                        <Text fontSize="36px">+</Text>
+                        <Text variant="body2">Upload Image</Text>
+                      </Flex>
+                    </label>
+                  </Flex>
                 </Stack>
               </Flex>
             </Box>
-            <LessonTable/>
+            <LessonTable />
           </Box>
-          
         </Flex>
       </Flex>
     </>
