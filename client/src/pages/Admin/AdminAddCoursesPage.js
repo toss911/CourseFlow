cf5bdeab7a8c1c24d6cabb21cd87eaec8eecdb06
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-11-22 14:51:33 +0700

    feat: add lesson section

--- client/src/pages/Admin/AdminAddCoursesPage.js
+++ client/src/pages/Admin/AdminAddCoursesPage.js
@@ -1,4 +1,22 @@
-import { Box, Flex, Stack, Input, Text, Container, Heading, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Stack,
+  Input,
+  Text,
+  Container,
+  Heading,
+  Button,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 import { Sidebar } from "../../components/SidebarAdmin";
 import AdminNavbarAdd from "../../components/AdminNavbarAdd";
@@ -8,7 +26,7 @@ const AdminAddCoursesPage = () => {
     <>
       <Flex>
         <Sidebar />
-        <Flex flexDirection="column" w="vw">
+        <Flex flexDirection="column" w="vw" backgroundColor="gray.100">
           <AdminNavbarAdd
             heading="Add Course"
             action="Cancel"
@@ -17,7 +35,7 @@ const AdminAddCoursesPage = () => {
           <Box
             alignItems="center"
             m="40px 40px 40px 40px"
-            backgroundColor="gray.100"
+            backgroundColor="white"
             px="100px"
             py="40px"
             h="vh"
@@ -54,18 +72,83 @@ const AdminAddCoursesPage = () => {
                 </Box>
               </Stack>
             </Flex>
-            {/* Lesson Section */}
-          <Flex flexDirection="column">
-            <Flex>
-              <Heading variant="headline3">Lesson</Heading>
+          </Box>
+          {/* Lesson Section */}
+          <Flex flexDirection="column" mt="29px" ml="39px">
+            <Flex gap="16px" alignItems="center">
+              <Heading variant="headline3" w="933.5px">
+                Lesson
+              </Heading>
               <Button variant="primary">+ Add Lesson</Button>
-
             </Flex>
 
+            <TableContainer borderRadius="8px" w="1120px" mt="43px" mb="58px">
+              <Table variant="simple" backgroundColor="white">
+                <Thead backgroundColor="gray.300">
+                  <Tr>
+                    <Th></Th>
+                    <Th></Th>
+                    <Th>
+                      <Text
+                        variant="body3"
+                        textTransform="capitalize"
+                        textColor="gray.800"
+                        w="468px"
+                      >
+                        Lesson name
+                      </Text>
+                    </Th>
+                    <Th>
+                      <Text
+                        variant="body3"
+                        textTransform="capitalize"
+                        textColor="gray.800"
+                        w="300px"
+                      >
+                        Sub-lesson
+                      </Text>
+                    </Th>
+                    <Th>
+                      <Text
+                        variant="body3"
+                        textTransform="capitalize"
+                        textColor="gray.800"
+                      >
+                        Action
+                      </Text>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>
+                      <Image
+                        src="../../../assets/admin-page/drag.svg"
+                        alt="drag"
+                      ></Image>
+                    </Td>
+                    <Td>1</Td>
+                    <Td>Blah blah blah</Td>
+                    <Td>Blah blah blah</Td>
+                    <Td>
+                      <Flex gap="20px" w="120px">
+                        <Image
+                          src="../../../assets/admin-page/bin.svg"
+                          alt="bin"
+                        ></Image>
+                        <Image
+                          src="../../../assets/admin-page/edit.svg"
+                          alt="edit"
+                        ></Image>
+                      </Flex>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
           </Flex>
-          </Box>
+          {/* End of Lesson Section */}
         </Flex>
-  
       </Flex>
     </>
   );
