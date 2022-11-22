daaeb069cf1043d20987d13bf354399d3b3b2ee5
Sundaeth <potsaweeth@gmail.com>
2022-11-22 14:43:55 +0700

    fix:background color

--- client/src/pages/Admin/AdminAddCoursesPage.js
+++ client/src/pages/Admin/AdminAddCoursesPage.js
@@ -1,4 +1,12 @@
-import { Box, Flex, Stack, Input, Text, Container } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Stack,
+  Input,
+  Text,
+  Container,
+  Image,
+} from "@chakra-ui/react";
 import React from "react";
 import { Sidebar } from "../../components/SidebarAdmin";
 import AdminNavbarAdd from "../../components/AdminNavbarAdd";
@@ -14,46 +22,48 @@ const AdminAddCoursesPage = () => {
             action="Cancel"
             action2="Create"
           />
-          <Box
-            alignItems="center"
-            m="40px 40px 40px 40px"
-            backgroundColor="gray.100"
-            px="100px"
-            py="40px"
-            h="vh"
-          >
-            <Flex>
-              <Stack
-                spacing="28px"
-                flexDirection="row"
-                justifyContent="center"
-                flexWrap="wrap"
-              >
-                <Box>
-                  <Text variant="body2">Course name *</Text>
-                  <Input placeholder="" size="lg" w="920px" />
-                </Box>
+          <Box backgroundColor="gray.100">
+            <Box
+              m="40px 40px 40px 40px"
+              px="100px"
+              pb="60px"
+              pt="40px"
+              h="vh"
+              bg="white"
+            >
+              <Flex>
+                <Stack
+                  spacing="28px"
+                  flexDirection="row"
+                  justifyContent="center"
+                  flexWrap="wrap"
+                >
+                  <Box>
+                    <Text variant="body2">Course name *</Text>
+                    <Input placeholder="" size="lg" w="920px" />
+                  </Box>
 
-                <Flex justifyContent="space-between" w="920px">
+                  <Flex justifyContent="space-between" w="920px">
+                    <Box>
+                      <Text variant="body2">Price *</Text>
+                      <Input placeholder="" size="lg" w="420px" />
+                    </Box>
+                    <Box>
+                      <Text variant="body2">Total learning time *</Text>
+                      <Input placeholder="" size="lg" w="420px" />
+                    </Box>
+                  </Flex>
                   <Box>
-                    <Text variant="body2">Price *</Text>
-                    <Input placeholder="" size="lg" w="420px" />
+                    <Text variant="body2">Course summary</Text>
+                    <Input placeholder="" h="72px" w="920px" />
                   </Box>
                   <Box>
-                    <Text variant="body2">Total learning time *</Text>
-                    <Input placeholder="" size="lg" w="420px" />
+                    <Text variant="body2">Course detail</Text>
+                    <Input placeholder="" h="192px" w="920px" />
                   </Box>
-                </Flex>
-                <Box>
-                  <Text>Course summary</Text>
-                  <Input placeholder="" h="72px" w="920px" />
-                </Box>
-                <Box>
-                  <Text>Course detail</Text>
-                  <Input placeholder="" h="192px" w="920px" />
-                </Box>
-              </Stack>
-            </Flex>
+                </Stack>
+              </Flex>
+            </Box>
           </Box>
         </Flex>
       </Flex>
