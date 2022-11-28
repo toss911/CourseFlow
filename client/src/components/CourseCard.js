a6da0ef1c99b9d478b955278d1fc96228701a34c
Tossaporn Udomsap <tossaporn.udom@gmail.com>
2022-11-29 01:03:35 +0700

    fix(course-card): changed the reference value of cover image

--- client/src/components/CourseCard.js
+++ client/src/components/CourseCard.js
@@ -28,7 +28,12 @@ export function CourseCard(props) {
       cursor="pointer"
     >
       <Box>
-        <Image w="357px" h="240px" objectFit="cover" src={props.courseImg} />
+        <Image
+          w="357px"
+          h="240px"
+          objectFit="cover"
+          src={props.courseImg.url}
+        />
       </Box>
       <Box>
         <Flex w="325px" h="115px" flexDirection="column" ml="16px">
