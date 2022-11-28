2fd5ffce4e452ccc544dcf19244fc352288369b7
Tossaporn Udomsap <tossaporn.udom@gmail.com>
2022-11-29 00:21:09 +0700

    fix(course-card): changed a resolution of cover image in course card to be fixed width/height

--- client/src/components/CourseCard.js
+++ client/src/components/CourseCard.js
@@ -27,8 +27,8 @@ export function CourseCard(props) {
       }}
       cursor="pointer"
     >
-      <Box w="357px" h="240px">
-        <Image w="357px" src={props.courseImg} />
+      <Box>
+        <Image w="357px" h="240px" objectFit="cover" src={props.courseImg} />
       </Box>
       <Box>
         <Flex w="325px" h="115px" flexDirection="column" ml="16px">
