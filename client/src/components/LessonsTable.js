851894ed169b78ba19409797a213bc82ad56eb25
Sundaeth <potsaweeth@gmail.com>
2022-11-28 10:37:25 +0700

    feat:compeleted drag and drop fucntion

--- client/src/components/LessonsTable.js
+++ client/src/components/LessonsTable.js
@@ -11,10 +11,53 @@ import {
   TableContainer,
   Text,
   Image,
+  Box,
+  List,
+  UnorderedList,
+  ListItem,
+  Container,
+  Grid,
+  GridItem,
 } from "@chakra-ui/react";
+import { DragHandleIcon } from "@chakra-ui/icons";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React from "react";
 
 const LessonTable = () => {
+  const data = [
+    {
+      id: 1,
+      title: "aaaaaaa",
+      name: "11 ",
+    },
+    {
+      id: 2,
+      title: "bbbssssvv",
+      name: "12 ",
+    },
+    {
+      id: 3,
+      title: "cccvvvvvvv",
+      name: "13",
+    },
+  ];
 
+  // list telling Dnd which items index
+  const [list, setList] = React.useState(data);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  // function that let Dnd reorder after drag
+  const onEnd = (result) => {
+    console.log(result);
+    setList(reorder(list, result.source.index, result.destination.index));
+  };
   return (
     <>
       <Flex flexDirection="column" mt="29px" ml="39px">
@@ -28,65 +71,121 @@ const LessonTable = () => {
         <TableContainer borderRadius="8px" w="1120px" mt="43px" mb="58px">
           <Table variant="simple" backgroundColor="white">
             <Thead backgroundColor="gray.300">
-              <Tr>
-                <Th></Th>
-                <Th></Th>
-                <Th>
-                  <Text
-                    variant="body3"
-                    textTransform="capitalize"
-                    textColor="gray.800"
-                    w="468px"
-                  >
-                    Lesson name
-                  </Text>
-                </Th>
-                <Th>
-                  <Text
-                    variant="body3"
-                    textTransform="capitalize"
-                    textColor="gray.800"
-                    w="300px"
-                  >
-                    Sub-lesson
-                  </Text>
-                </Th>
-                <Th>
-                  <Text
-                    variant="body3"
-                    textTransform="capitalize"
-                    textColor="gray.800"
-                  >
-                    Action
-                  </Text>
-                </Th>
-              </Tr>
+              <Grid templateColumns="repeat(13, 1fr)" gap={20}>
+                <GridItem colSpan={2} h="10"></GridItem>
+                <GridItem
+                  colSpan={1}
+                  // bg="red.100"
+                  alignSelf="center"
+                  justifySelf="start"
+                  ml="10px"
+                  variant="body3"
+                >
+                  Lesson name
+                </GridItem>
+
+                <GridItem
+                  colSpan={5}
+                  alignSelf="center"
+                  justifySelf="end"
+                  variant="body3"
+                >
+                  Sub-lesson
+                </GridItem>
+
+                <GridItem
+                  colSpan={5}
+                  justifySelf="end"
+                  alignSelf="center"
+                  mr="50px"
+                  variant="body3"
+                >
+                  Action
+                </GridItem>
+              </Grid>
             </Thead>
-            <Tbody>
-              <Tr>
-                <Td>
-                  <Image
-                    src="../../../assets/admin-page/drag.svg"
-                    alt="drag"
-                  ></Image>
-                </Td>
-                <Td>1</Td>
-                <Td>Blah blah blah</Td>
-                <Td>Blah blah blah</Td>
-                <Td>
-                  <Flex gap="20px" w="120px">
-                    <Image
-                      src="../../../assets/admin-page/bin.svg"
-                      alt="bin"
-                    ></Image>
-                    <Image
-                      src="../../../assets/admin-page/edit.svg"
-                      alt="edit"
-                    ></Image>
-                  </Flex>
-                </Td>
-              </Tr>
-            </Tbody>
+
+            {/* // dnd start here onEnd to tell Dnd to stop after </DragDropContext> */}
+            <Flex>
+              <DragDropContext onDragEnd={onEnd}>
+                <Droppable droppableId="1234" type="PERSON">
+                  {(provided, snapshot) => (
+                    <Box
+                      flexWrap="wrap"
+                      colSpan="3"
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      {list.map((item, index) => (
+                        <Draggable
+                          colSpan="3"
+                          draggableId={item.id.toString()}
+                          index={index}
+                          key={item.id}
+                        >
+                          {(provided, snapshot) => (
+                            <Box
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              display="flex"
+                              colSpan="3"
+                              flexDirection="row"
+                            >
+                              <Tbody colSpan={3}>
+                                <Tr>
+                                  <Td>
+                                    <DragHandleIcon></DragHandleIcon>
+                                  </Td>
+
+                                  <Td>{item.id}</Td>
+                                  <Grid
+                                    templateColumns="repeat(16, 6fr)"
+                                    gap={10}
+                                  >
+                                    <GridItem colSpan={1} h="10"></GridItem>
+                                    <GridItem alignSelf="center" colSpan={4}>
+                                      {item.title}
+                                    </GridItem>
+                                    <GridItem colSpan={3}></GridItem>
+                                    <GridItem
+                                      alignSelf="center"
+                                      justifySelf="start"
+                                      colSpan={1}
+                                      mr="40px"
+                                    >
+                                      {item.name}
+                                    </GridItem>
+                                    <GridItem colSpan={5}></GridItem>
+                                    <GridItem
+                                      display="flex"
+                                      alignSelf="center"
+                                      gap="15px"
+                                    >
+                                      <Image
+                                        src="../../../assets/admin-page/bin.svg"
+                                        alt="bin"
+                                      ></Image>
+
+                                      <Image
+                                        src="../../../assets/admin-page/edit.svg"
+                                        alt="edit"
+                                      ></Image>
+                                    </GridItem>
+                                  </Grid>
+                                </Tr>
+                              </Tbody>
+                            </Box>
+                          )}
+                        </Draggable>
+                      ))}
+                      {/* placeholder is the container let Dnd know where the end of drag is */}
+                      {provided.placeholder}
+                    </Box>
+                  )}
+                </Droppable>
+              </DragDropContext>
+            </Flex>
           </Table>
         </TableContainer>
       </Flex>
--- client/src/index.js
+++ client/src/index.js
@@ -7,24 +7,24 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "./contexts/authentication.js";
 import theme from "./configs/theme.js";
 import jwtInterceptor from "./utils/jwtInterceptors.js";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppAdmin from "./AppAdmin.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 jwtInterceptor();
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <AuthProvider>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
         <Routes>
-          <Route path="/*" element={<App />}/>
-          <Route path="/admin/*" element={<AppAdmin/>}/>
+          <Route path="/*" element={<App />} />
+          <Route path="/admin/*" element={<AppAdmin />} />
         </Routes>
-        </AuthProvider>
-      </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+      </AuthProvider>
+    </ChakraProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
