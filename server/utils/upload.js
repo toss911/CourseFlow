29e1d40fa29b09a5de2dd1f27806828bd3041626
Yanaphat Poonkrasee <yanaphat339@gmail.com>
2022-12-02 11:58:55 +0700

    fix: fixed cloudinary delete func to allow other files type delete other than image

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,29 +1,47 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs/promises";
 
-const cloudinaryUpload = async (file, action, nameFolder) => {
+const cloudinaryUpload = async (file, action, nameFolder, fileType) => {
   try {
     if (/upload/i.test(action)) {
       const result = await cloudinary.uploader.upload(file.path, {
         folder: `courseflow/${nameFolder}`,
         type: "private",
-        resource_type: "auto"
+        resource_type: "auto",
       });
       await fs.unlink(file.path);
       return { url: result.secure_url, public_id: result.public_id };
     } else if (/delete/i.test(action)) {
-      await cloudinary.uploader.destroy(file, {
-        type: "private",
-        resource_type: "raw"
-      });
-      // await cloudinary.v2.api.delete_resources(file, {
-      //   type: "private",
-      // })
+      if (/image/i.test(file)) {
+        await cloudinary.uploader.destroy(file, {
+          type: "private",
+        });
+      } else if (/video/i.test(file)) {
+        await cloudinary.uploader.destroy(file, {
+          type: "private",
+          resource_type: "video",
+        });
+      } else if (/file/i.test(file)) {
+        if (/image/i.test(fileType)) {
+          await cloudinary.uploader.destroy(file, {
+            type: "private",
+          });
+        } else if (/video/i.test(fileType)) {
+          await cloudinary.uploader.destroy(file, {
+            type: "private",
+            resource_type: "video",
+          });
+        } else {
+          await cloudinary.uploader.destroy(file, {
+            type: "private",
+            resource_type: "raw",
+          });
+        }
+      }
     }
   } catch (error) {
-    console.log("Error from uploading:", error);
+    console.log("Error from uploading/ deleting:", error);
   }
 };
-
 
 export { cloudinaryUpload };
